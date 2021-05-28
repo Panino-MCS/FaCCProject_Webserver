90a5317daf30ef4efcae13a400504e8ab06f6988
Panino-MCS <andrea.traldi@studenti.unitn.it>
2021-05-28 17:20:03 +0200

    COmmit

---
+++ Webserver.java
@@ -0,0 +1,39 @@
+
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+public class Webserver {
+
+    public static void main(String[] args) throws Exception {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+        server.createContext("/game", new MyHandler());
+        server.setExecutor(null); // creates a default executor
+        server.start();
+    }
+
+    static class MyHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+            
+            //Execute command
+            try {
+                Process process = Runtime.getRuntime().exec("kubectl run -it --rm --image=docker.io/andreatraldi/game:v1");
+            } catch (Exception e) {}
+    
+
+
+            String response = "Entering the Game!";
+            t.sendResponseHeaders(200, response.length());
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+
+}
